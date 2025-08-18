5b5dbc358df81b27768df44a3f6e68eb6cf4f2ef
rzlpil <rizalrahmanspil@gmail.com>
2025-08-18 09:34:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import streamlit as st
 pages = {
     "Menu": [
         st.Page("aehsdport.py", title="A/E HSD at Port"),
+        st.Page("aehsdporttanpaduration.py", title="A/E HSD at Port tanpa duration"),
         st.Page("memfo.py", title="M/E MFO"),
     ]
 }
